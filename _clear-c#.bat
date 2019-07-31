dedf975517f8e00c6ec835119962db930de9fde1
Eugene <evgeniy.alyabyev@loginom.ru>
2019-07-31 19:05:31 +0300

    update utils files

--- _clear-c#.bat
+++ _clear-c#.bat
@@ -1,8 +1,15 @@
 @echo off
+
+echo deleting '.vs' folders...
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S .vs') DO RMDIR /S /Q "%%G"
+@echo    done.
+
 echo deleting 'bin' folders...
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
 @echo    done.
+
 echo deleting 'obj' folders...
 FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
 @echo    done.
+
 pause
